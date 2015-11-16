d4f55cc506eda9c0f8cb573e60a7bcdbe582b972
clairew17 <xuw15@pitt.edu>
2015-11-15 23:52:34 -0500

    test

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ import matplotlib
 
 matplotlib.style.use('ggplot')
 
-ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-ts = ts.cumsum()
-ts.plot()
+#ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+#ts = ts.cumsum()
+
+fig = plt.figure()
+#ts.plot()
+
+fig.savefig("test.pdf")
